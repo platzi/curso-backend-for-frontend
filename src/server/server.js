5834c15a9e7985677dc16b67c9a695fa57a73717
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-25 09:22:10 -0500

    Version control is awful

--- src/server/server.js
+++ src/server/server.js
@@ -16,10 +16,22 @@ import initialState from '../frontend/initialState';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import getManifest from './getManifest';
 
+import cookieParser from 'cookie-parser';
+import boom from '@hapi/boom';
+import passport from 'passport';
+import axios from 'axios';
+
 dotenv.config();
 
 const app = express();
 const { ENV, PORT } = process.env;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./utils/auth/strategies/basic');
 
 if (ENV === 'development') {
   const webPackConfig = require('../../webpack.config');
@@ -81,6 +93,50 @@ const renderApp = (req, res) => {
   )
   res.send(setResponse(html, preloadedState, req.hashManifest));
 };
+
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function (req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 app.get('*', renderApp);
 
