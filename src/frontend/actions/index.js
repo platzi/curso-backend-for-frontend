fa8db7432c9f35f4956ad83ad7def837a451ea52
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-25 12:25:11 -0500

    This commit is a lie

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -60,7 +60,6 @@ export const loginUser = ({ email, password }, redirectUrl) => {
         document.cookie = `email=${data.user.email}`;
         document.cookie = `name=${data.user.name}`;
         document.cookie = `id=${data.user.id}`;
-        document.cookie = `token=${data.user.token}`
         dispatch(loginRequest(data.user));
       })
       .then(() => {
--- src/server/server.js
+++ src/server/server.js
@@ -78,20 +78,26 @@ const setResponse = (html, preloadedState, manifest) => {
   );
 };
 
-const renderApp = (req, res) => {
+const renderApp = async (req, res) => {
   let initialState;
-  const { email, name, id } = req.cookies;
+  const { token, email, name, id } = req.cookies;
 
-  if (id) {
+  try {
+    let movieList = await axios({
+      url: `${process.env.API_URL}/api/movies`,
+      headers: { Authorization: `Bearer ${token}`},
+      method: 'get',
+    });
+    movieList = movieList.data.data;
     initialState = {
       user: {
-        email, name, id
+        id, email, name,
       },
       myList: [],
-      trends: [],
-      originals: []
-    }
-  } else {
+      trends: movieList.filter(movie => movie.contentRating === 'PG' &&movie._id),
+      originals: movieList.filter(movie => movie.contentRating === 'G'&& movie._id)
+    };
+  } catch (err) {
     initialState = {
       user: {},
       myList: [],
