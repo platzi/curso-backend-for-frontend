51b2adc5002e8a63913db45092e123e63994a3d4
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-25 09:33:55 -0500

    Reinventing the wheel. Again.

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 import App from './routes/App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const preloadedState = window.__PRELOADED_STATE__;
-const store = createStore(reducer, preloadedState, composeEnhancers());
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
 const history = createBrowserHistory();
 
 delete window.__PRELOADED_STATE__;
